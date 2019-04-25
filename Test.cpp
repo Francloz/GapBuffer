8787dd04f4c053ab8a290ff6858cfc23d79d7eb0
Francloz <33878222+Francloz@users.noreply.github.com>
2019-04-25 20:01:45 +0200

    Update and rename Source.cpp to Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -3,17 +3,15 @@
 #include <iostream>
 int main() {
 	GapBuffer gb;
-	gb.insert("Hola mundo ", 11);
+	gb.insert("Hello world ", 11);
 	std::cout << gb.get_string() << '\n';
-	gb.stable_insert("patata brava", 12);
+	gb.stable_insert("meaningless rubbish", 19);
 	std::cout << gb.get_string() << '\n';
 	gb.move_cursor(11);
 	std::cout << gb.get_string() << '\n';
 	gb.erase(7);
 	std::cout << gb.get_string() << '\n';
-	gb.insert("patata ", 7);
-	std::cout << gb.get_string() << '\n';
-
+	gb.insert("potato ", 7);
 	std::cout << gb.get_string() << '\n';
 	return 0;
 }
